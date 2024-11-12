3f99e33e3a55edee8333bd241ed1fe84da224db5
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-12 09:34:23 +0900

    first commit

---
+++ src/test/java/com/example/peep/PeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
