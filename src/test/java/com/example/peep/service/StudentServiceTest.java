1ffdecc9c34f3cc4fa12644560625e15d3b8ff0d
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-20 15:10:08 +0900

    #3 - test: coin, photo 저장하는지에 대한 테스트

--- src/test/java/com/example/peep/service/StudentServiceTest.java
+++ src/test/java/com/example/peep/service/StudentServiceTest.java
@@ -1,19 +1,22 @@
 package com.example.peep.service;
 
+import com.example.peep.domain.Coin;
+import com.example.peep.domain.Photo;
 import com.example.peep.domain.School;
 import com.example.peep.domain.Student;
 import com.example.peep.dto.SchoolDto;
 import com.example.peep.dto.StudentDto;
+import com.example.peep.repository.CoinRepository;
+import com.example.peep.repository.PhotoRepository;
 import com.example.peep.repository.SchoolRepository;
 import com.example.peep.repository.StudentRepository;
-import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -23,17 +26,18 @@ import static org.mockito.BDDMockito.*;
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
-    @Mock
-    private StudentRepository studentRepository;
-
-    @Mock
-    private SchoolRepository schoolRepository;
+    @Mock private StudentRepository studentRepository;
+    @Mock private SchoolRepository schoolRepository;
+    @Mock private PasswordEncoder passwordEncoder;
+    @Mock private CoinRepository coinRepository;
+    @Mock private PhotoRepository photoRepository;
 
     @InjectMocks
     private StudentService studentService;
 
+    @DisplayName("회원가입 후 저장 확인")
     @Test
-    void given_when_then() {
+    void givenStudent_whenNewStudent_thenGetStudent() {
 
         //Given
         SchoolDto schoolDto = SchoolDto.of(
@@ -52,28 +56,14 @@ class StudentServiceTest {
         );
         //When
         given(schoolRepository.getReferenceById(1L)).willReturn(School.of("광주광역시교육청","광주고등학교"));
-        given(studentRepository.findByUserId("wlgns")).willReturn(Optional.of(Student.of("wlgns", "1234", "지훈", null, 3, 1, "1234-1234")));
+        given(studentRepository.findByUserId("wlgns")).willReturn(Optional.of(Student.of("wlgns", "1234", "지훈", null, null, null, 3, 1, "1234-1234")));
+        given(passwordEncoder.encode("1234")).willReturn("$2a$10$9mAxfCvr.rzFJMTm/xBBse2/o42tV9dCwxQw4XAjNOAQXL9tIDjKC");
         studentService.newStudent(studentDto);
+
         //Then
         Student student = studentRepository.findByUserId("wlgns").get();
         assertThat(student.getUserId()).isEqualTo("wlgns");
         then(studentRepository).should().findByUserId("wlgns");
-
-//        //Given
-//        StudentDto studentDto = StudentDto.of(
-//                "wlgns",
-//                "1234",
-//                "지훈",
-//                null,
-//                3,
-//                1,
-//                "010-1234-1234"
-//        );
-//
-//        studentService.newStudent(studentDto);
-//
-//        assertThat(studentRepository.findByUserId("wlgns").get().getUserId()).isEqualTo("wlgns");
-
     }
 
 }
