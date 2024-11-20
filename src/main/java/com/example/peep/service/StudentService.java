b8ecd2079753dca60cd5528c9025133620a61f55
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-20 15:05:26 +0900

    #3 - feat: 비밀번호 암호화
    처음 생성하는 회원의 비밀번호 암호화와 coin, photo를 생성하여 저장

--- src/main/java/com/example/peep/service/StudentService.java
+++ src/main/java/com/example/peep/service/StudentService.java
@@ -1,16 +1,19 @@
 package com.example.peep.service;
 
+import com.example.peep.domain.Coin;
+import com.example.peep.domain.Photo;
 import com.example.peep.domain.School;
 import com.example.peep.domain.Student;
 import com.example.peep.dto.StudentDto;
+import com.example.peep.repository.CoinRepository;
+import com.example.peep.repository.PhotoRepository;
 import com.example.peep.repository.SchoolRepository;
 import com.example.peep.repository.StudentRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLOutput;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,15 +23,24 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
     private final SchoolRepository schoolRepository;
+    private final CoinRepository coinRepository;
+    private final PhotoRepository photoRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void newStudent(StudentDto studentDto) {
 
         Long schoolId = studentDto.schoolDto().id();
-        School school = schoolRepository.getReferenceById(schoolId);
-        Student student = studentDto.toEntity(school);
 
-        //TODO: 코인, 사진 데이터 만들기
-        //TODO: 비밀번호 암호화
+        School school = schoolRepository.getReferenceById(schoolId);
+        Coin coin = Coin.of(0);
+        Photo photo = Photo.of(null);
+        Student student = studentDto.toEntity(school, coin, photo);
+
+        String hashedPassword = passwordEncoder.encode(student.getUserPassword());
+        student.setUserPassword(hashedPassword);
+
+        coinRepository.save(coin);
+        photoRepository.save(photo);
         studentRepository.save(student);
     }
 
