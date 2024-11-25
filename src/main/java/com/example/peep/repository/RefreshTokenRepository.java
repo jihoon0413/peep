512e0713a0d10ff559806cbb8f9176be88d24c53
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-25 22:19:19 +0900

    #5 - feat: 로그아웃 시 refrestToken삭제
    로그아웃 시 refrestToken삭제하는 jpa추가

--- src/main/java/com/example/peep/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/peep/repository/RefreshTokenRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     RefreshToken findByUserIdAndDeviceId(String userId, String deviceId);
+    void deleteByUserIdAndToken(String userId, String refreshToken);
 
 }
