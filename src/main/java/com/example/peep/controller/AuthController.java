a8257ba4e99279bf81394b2bcc9ea3b9c317ca47
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-12-03 10:37:27 +0900

    #18 - feat: 로그인, 로그아웃 기록 저장

--- src/main/java/com/example/peep/controller/AuthController.java
+++ src/main/java/com/example/peep/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.example.peep.dto.JwtTokenDto;
 import com.example.peep.dto.StudentDto;
 import com.example.peep.dto.requestDto.VerifyCodeRequestDto;
 import com.example.peep.service.AuthService;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +20,9 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public JwtTokenDto login(@RequestHeader("Device-Id") String deviceId,
+    public JwtTokenDto login(HttpServletRequest request,
                              @RequestBody StudentDto studentDto) {
-        return authService.login(deviceId, studentDto);
+        return authService.login(request, studentDto);
     }
 
     @PostMapping("/refresh")
@@ -30,16 +32,17 @@ public class AuthController {
                                     @RequestParam("userId") String userId) {
 
         String oldAccess = accessToken.substring(7);
-        return authService.refreshToken(deviceId, userId, jwtTokenDto, oldAccess);
+        return authService.refreshToken(deviceId, jwtTokenDto, oldAccess);
     }
 
     @PostMapping("/logout")
-    public String logout(@RequestBody JwtTokenDto jwtTokenDto,
+    public String logout(HttpServletRequest request,
+                         @RequestBody JwtTokenDto jwtTokenDto,
                          @RequestHeader("Authorization") String accessToken) {
 
         String oldAccess = accessToken.substring(7);
 
-        authService.logout(jwtTokenDto, oldAccess);
+        authService.logout(request, jwtTokenDto, oldAccess);
         return "Logout successful";
     }
 
--- src/main/java/com/example/peep/service/AuthService.java
+++ src/main/java/com/example/peep/service/AuthService.java
@@ -3,16 +3,22 @@ package com.example.peep.service;
 import com.example.peep.config.jwt.JwtToken;
 import com.example.peep.config.jwt.JwtTokenProvider;
 import com.example.peep.details.SmsUtil;
+import com.example.peep.domain.LoginRecord;
 import com.example.peep.domain.RefreshToken;
+import com.example.peep.dto.Event;
 import com.example.peep.dto.JwtTokenDto;
 import com.example.peep.dto.StudentDto;
 import com.example.peep.dto.requestDto.VerifyCodeRequestDto;
+import com.example.peep.repository.LoginRecordRepository;
 import com.example.peep.repository.RefreshTokenRepository;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -29,29 +35,32 @@ public class AuthService {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final LoginRecordRepository loginRecordRepository;
     private final TokenBlacklistService tokenBlacklistService;
     private final VerificationCodeService verificationCodeService;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SmsUtil smsUtil;
 
-    public JwtTokenDto login(String deviceId, StudentDto studentDto) {
+    public JwtTokenDto login(HttpServletRequest request, StudentDto studentDto) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(studentDto.userId(), studentDto.userPassword());
 
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
-        return JwtTokenDto.from(jwtTokenProvider.generateToken(authentication, studentDto.userId(), deviceId));
+        recordEvent(request, studentDto.userId(), Event.LOG_IN);
+
+        return JwtTokenDto.from(jwtTokenProvider.generateToken(authentication, studentDto.userId(), request.getHeader("Device-Id")));
 
     }
 
-    public JwtTokenDto refreshToken(String deviceId, String userId, JwtTokenDto jwtTokenDto, String oldAccess) {
+    public JwtTokenDto refreshToken(String deviceId, JwtTokenDto jwtTokenDto, String oldAccess) {
 
-        RefreshToken refresh = refreshTokenRepository.findByUserIdAndDeviceId(userId, deviceId);
+        RefreshToken refresh = refreshTokenRepository.findByUserIdAndDeviceId(jwtTokenDto.id(), deviceId);
 
         String accessToken = "";
 
         try {
             if (refresh != null && jwtTokenProvider.validateToken(jwtTokenDto.refreshToken()) && jwtTokenDto.refreshToken().contains(refresh.getToken())) {
-                accessToken = jwtTokenProvider.generateAccess(userId);
+                accessToken = jwtTokenProvider.generateAccess(jwtTokenDto.id());
             } else {
                 throw new IllegalArgumentException();
             }
@@ -60,14 +69,14 @@ public class AuthService {
             return null;
         }
         tokenBlacklistService.addToBlacklist(oldAccess);
-        JwtToken jwtToken = new JwtToken("Bearer", accessToken, refresh.getToken(), userId);
+        JwtToken jwtToken = new JwtToken("Bearer", accessToken, refresh.getToken(), jwtTokenDto.id());
 
         return JwtTokenDto.from(jwtToken);
     }
 
-    public void logout(JwtTokenDto jwtTokenDto, String oldAccess) {
+    public void logout(HttpServletRequest request, JwtTokenDto jwtTokenDto, String oldAccess) {
 
-        long now = (new Date()).getTime();
+        recordEvent(request, jwtTokenDto.id(), Event.LOG_OUT);
 
         tokenBlacklistService.addToBlacklist(oldAccess);
 
@@ -90,4 +99,34 @@ public class AuthService {
 
         return verificationCodeService.verifyCode(verifyCodeRequestDto.phoneNumber(), verifyCodeRequestDto.verifyCode());
     }
+
+    public void recordEvent(HttpServletRequest request, String userId, Event eventType) {
+
+        String userAgent = getUserAgent(request);
+        String clientIp = getIp(request);
+
+        LoginRecord loginRecord = LoginRecord.of(userId, clientIp, userAgent, eventType);
+
+        loginRecordRepository.save(loginRecord);
+    }
+
+    public String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public String getUserAgent(HttpServletRequest request) {
+        return request.getHeader("User-Agent");
+    }
+
 }
+
