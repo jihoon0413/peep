cd1ab3e9f236d6225056aebe97aecbf4e8316841
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-12-08 15:32:08 +0900

    #22 - chore: 쓰지 않은 jpa삭제

--- src/main/java/com/example/peep/repository/FollowRepository.java
+++ src/main/java/com/example/peep/repository/FollowRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFollowerUserIdAndFollowingUserId(String follower, String following);
-    void deleteByFollowerUserIdAndFollowingUserId(String follower, String following);
     List<Follow> findAllByFollowerUserId(String userId);
     List<Follow> findAllByFollowingUserId(String userId);
 }
