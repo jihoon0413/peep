46b83bf070a60047baa4ec06c94e315dcdd64221
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-12-08 15:32:45 +0900

    #22 - chore: 쓰지 않은 라이브러리 제거

--- src/main/java/com/example/peep/domain/AuditingFields.java
+++ src/main/java/com/example/peep/domain/AuditingFields.java
@@ -1,18 +1,13 @@
 package com.example.peep.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Builder;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDateTime;
 
 
