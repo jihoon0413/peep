bb8b1811f5a1f7b0314c97fca8bce328d69c89fe
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-12-11 13:28:19 +0900

    #3 - chore: 학생response 수정

--- src/main/java/com/example/peep/dto/response/StudentResponse.java
+++ src/main/java/com/example/peep/dto/response/StudentResponse.java
@@ -1,23 +1,27 @@
 package com.example.peep.dto.response;
 
 import com.example.peep.domain.Student;
+import com.example.peep.dto.HashtagDto;
 import com.example.peep.dto.PhotoDto;
 import com.example.peep.dto.SchoolDto;
+
+import java.util.List;
 
 public record StudentResponse(
     String userId,
     String name,
     SchoolDto schoolDto,
     PhotoDto photoDto,
+    List<HashtagDto> hashtagDtos,
     int grade,
     int myClass
 ) {
         public static StudentResponse of(String userId, String name, SchoolDto schoolDto, int grade, int myClass) {
-            return new StudentResponse(userId, name, schoolDto, null, grade, myClass);
+            return new StudentResponse(userId, name, schoolDto, null, null, grade, myClass);
         }
 
-        public static StudentResponse of(String userId, String name, SchoolDto schoolDto, PhotoDto photoDto, int grade, int myClass) {
-            return new StudentResponse(userId, name, schoolDto, photoDto, grade, myClass);
+        public static StudentResponse of(String userId, String name, SchoolDto schoolDto, PhotoDto photoDto, List<HashtagDto> hashtagDtos, int grade, int myClass) {
+            return new StudentResponse(userId, name, schoolDto, photoDto, hashtagDtos, grade, myClass);
         }
 
         public static StudentResponse from(Student student) {
@@ -26,6 +30,9 @@ public record StudentResponse(
                     student.getName(),
                     SchoolDto.from(student.getSchool()),
                     PhotoDto.from(student.getPhoto()),
+                    student.getHashtags().stream().map(studentHashtag -> {
+                        return HashtagDto.from(studentHashtag.getHashtag());
+                    }).toList(),
                     student.getGrade(),
                     student.getMyClass()
             );
