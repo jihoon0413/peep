8a76e35e5114c16a988eeec7f4dbc864502e63e2
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2025-01-07 20:09:10 +0900

    생성자추가

--- src/main/java/com/example/peep/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/peep/config/jwt/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import com.example.peep.repository.RefreshTokenRepository;
 import com.example.peep.service.TokenBlacklistService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +25,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     private final Key key;
